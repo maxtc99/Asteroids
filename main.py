12256a8370b03e1c13f93c16153916948a52df58
maxtc99 <max.tc99@gmail.com>
2025-10-27 17:55:40 +0000

    draw player and set hitbox

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import pygame
 from constants import *
+from player import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
 
-def draw_screen():
-    clock = pygame.time.Clock()
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()  #fps
     dt = 0
+
+    print(f'''Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}''')
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        
+
+        player.draw(screen)
+
+
+
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
-        
-        
-
-
-def main():
-    pygame.init()
-    print(f'''Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}''')
-    draw_screen()
-
-
+    
+    
 
 if __name__ == "__main__":
     main()
