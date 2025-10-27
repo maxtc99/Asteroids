893d11cb3afaae3d7018d8b830bedc64a4d74b61
maxtc99 <max.tc99@gmail.com>
2025-10-27 16:46:09 +0000

    Draw game onto screen

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 from constants import *
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print('''Starting Asteroids!\nScreen width: 1280\nScreen height: 720''')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    
+    
+    
 
 
 if __name__ == "__main__":
