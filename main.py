8a84611d0907c560973351e2185508ea699b85f5
maxtc99 <max.tc99@gmail.com>
2025-10-27 18:18:40 +0000

    added move function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    clock = pygame.time.Clock()  #fps
+    clock = pygame.time.Clock()  
     dt = 0
 
     print(f'''Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}''')
@@ -17,16 +17,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
+
         screen.fill("black")
-        
-        
-
         player.draw(screen)
-
-
-
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+
+        dt = clock.tick(60) / 1000  # 60fps
     
     
 
