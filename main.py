f54b6fa5f3669d6d78b9eca6852404390f66c599
maxtc99 <max.tc99@gmail.com>
2025-10-27 17:16:42 +0000

    added clock funciton to lock fps at 60

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
 from constants import *
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print('''Starting Asteroids!\nScreen width: 1280\nScreen height: 720''')
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def draw_screen():
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-    
-    
-    
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
+        
+
+
+def main():
+    pygame.init()
+    print(f'''Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}''')
+    draw_screen()
+
 
 
 if __name__ == "__main__":
